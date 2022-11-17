90bca3ec3b350b5a44eeaddda7de5895cead20c7
Aina <ainamoreno@outlook.com>
2022-11-17 08:49:02 +0100

    Corrigiendo del merge y añadiendo estilos

--- src/Components/Serie/Serie.jsx
+++ src/Components/Serie/Serie.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Serie.css";
 import { useNavigate } from "react-router-dom";
+import { Col, Row } from "react-bootstrap";
 // import Details from "../Details/Details";
 
 function Serie({ serie }) {
@@ -13,21 +14,18 @@ function Serie({ serie }) {
     setId(param);
   };
   return (
-    <>
-      {serie.map((mov, index) => (
-        <div
-          key={index}
-          className="divSerie"
-          onClick={() => detailsSerie(mov.id_article)}
-        >
-          <p>{mov.id_article}</p>
-          <h4>{mov.name}</h4>
-          <img className="imgSerie" src={`${mov.photo}`} alt="" />
-        </div>
-      ))}
-
-      {/* {hasRender && <Details id={id} serie={serie} />} */}
-    </>
+    <Row>
+          {serie.map((ser, index) => (
+            <Col
+              key={index}
+              className="divMovie"
+            >
+              <img className="imgMovie" src={`${ser.photo}`} alt="" />
+              <h6>{ser.name}</h6>
+            </Col>
+          ))}
+        
+      </Row>
   );
 }
 

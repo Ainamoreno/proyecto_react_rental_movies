489080263176827c619c5d9f347bc16064c10173
salva-martinez <salvamartinezjuan@hotmail.com>
2022-11-24 12:06:00 +0100

    browser

--- src/Components/Serie/Serie.jsx
+++ src/Components/Serie/Serie.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 import { Col, Row } from "react-bootstrap";
 // import Details from "../Details/Details";
 
+import Browser from "../../Components/Browser/Browser";
+
 function Serie({ serie }) {
   let navigate = useNavigate();
   const [hasRender, setRender] = useState(false);
@@ -14,18 +16,21 @@ function Serie({ serie }) {
     setId(param);
   };
   return (
+    <div>
+    
     <Row>
-          {serie.map((ser, index) => (
-            <Col
-              key={index}
-              className="divMovie"
-            >
-              <img className="imgMovie" src={`${ser.photo}`} alt="" />
-              <h6>{ser.name}</h6>
-            </Col>
-          ))}
-        
-      </Row>
+      {serie.map((ser, index) => (
+        <Col
+        key={index}
+        className="divMovie"
+        >
+          <img className="imgMovie" src={`${ser.photo}`} alt="" />
+          <h6>{ser.name}</h6>
+        </Col>
+      ))}
+
+    </Row>
+      </div>
   );
 }
 

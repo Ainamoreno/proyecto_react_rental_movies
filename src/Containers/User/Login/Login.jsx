993438cfe9209832a3a0958a9c93a272adb4afd0
Aina <ainamoreno@outlook.com>
2022-12-04 19:55:32 +0100

    --

--- src/Containers/User/Login/Login.jsx
+++ src/Containers/User/Login/Login.jsx
@@ -62,10 +62,12 @@ const Login = () => {
         setUser({ ...user, message: "Debes rellenar todos los datos" });
       } else {
         loginUser(user).then((res) => {
+          console.log(res.data.jwt)
           if(res.data.message === 'La contraseña o el email son incorrectos'){
             setUser({ ...user, message: 'La contraseña o el email son incorrectos' });
           }
           let jwt = res.data.jwt;
+          console.log(res.data.jwt)
           localStorage.setItem('TOKEN', JSON.stringify(jwt));
           const payload = decode(jwt);
           localStorage.setItem('CREDENTIALS', JSON.stringify(payload));
